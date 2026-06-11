7486f650743fa88f3563b3c6f74f05c68c23e9c7
alumno <51085860@ivt.edu.ar>
2026-06-11 10:23:20 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+let numero1 = 5
+let numero2 = 10
+
+let suma = numero1 + numero2 
+console.log (numero1 + "+" numero2 + "=" + suma)
+
+let resta = numero1 - numero2 
+console.log (numero1 + "-" + numero2 "=" + resta)
+
+let multiplicación = numero1 * numero2
+console.log (numero1 + "*" + numero2 "=" + multiplicación)
+
+let división = numero1 / numero2 
+console.log (numero1 + "/" + numero2 "=" + división)
+
+let nombrealumno = "benja"
+let apellidoalumno = "lopez"
+let edadalumno = 18
+console.log ("el alumno :" + nombrealumno + apellidoalumno +"tiene" + edadalumno )
+
+let precio1 = 6800
+let precio2 = 5500
+let precio3 = 1000
+let total = precio1 + precio2 + precio3 
+console.log ("el total a pagar es :" + total)
+
+let pais = "Argentina"
+console.log (pais)
+pais = "Brasil"
+console.log (pais)
+
+let comidafavorita = "sorrentinos de jamon y queso"
+let colorfavorito = "azul marino"
+let numerofavorito = "666"
