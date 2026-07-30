5b1d6b41ac9b941875989774d4330a0acbe54b04
alumno <51085860@ivt.edu.ar>
2026-07-30 10:43:43 -0300

    29/07

---
+++ TPN6/app.js
@@ -0,0 +1,14 @@
+let boton1 = document.queryselector('#BTN1')
+let boton2 = document.queryselector('#BTN2')
+let boton3 = document.queryselector('#BTN3')
+let boton4 = document.queryselector('#BTN4')
+let parrafo = document.querySelector(parrafo)
+
+boton1.onclick = function(){
+    "parrafo".text.content = "este texto ha sido modificado"
+
+
+}
+
+
+
